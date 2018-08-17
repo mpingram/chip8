a4d2bf4d2b70df6ca488710b172140cf9a8ef71b
Michael Ingram <mpingram92@gmail.com>
2018-08-16 23:50:06 -0500

    Fix bugs in instruction handling
    * fix indexing error in drawSprite (accessing sprite rows
      using same index as screen rows)
    * fix (hilarious) error reading opcodes
      ** had been reading the high byte twice of each opcode
        rather than the high and low bytes. I was looking at
        the logs and saying, 'Wow, whoever programmed this pong
        rom was an aesthetic, all of the opcodes have the same
        high and low byte!'

--- chip8.go
+++ chip8.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"golang.org/x/crypto/ssh/terminal"
 	"io/ioutil"
+	"log"
 	"math/rand"
 	"os"
 )
@@ -53,12 +55,15 @@ type Chip8 struct {
 	// F E D C B A 9 8 7 6 5 4 3 2 1 0
 	keyState uint16
 
-	DEBUG_INSTRS []string
+	log    bytes.Buffer
+	logger *log.Logger
 }
 
 const NULL_OPCODE uint16 = 0x0000
 
 func (c *Chip8) Run(program []byte) {
+
+	c.logger = log.New(&c.log, "chip8:", log.Ltime|log.Lmicroseconds)
 
 	// FIXME move elsewhere
 	// -----------------
@@ -66,8 +71,9 @@ func (c *Chip8) Run(program []byte) {
 	if err != nil {
 		panic(err)
 	}
-	defer terminal.Restore(0, oldState)
+	defer c.log.WriteTo(os.Stdout)
 	defer c.clearDisplay()
+	defer terminal.Restore(0, oldState)
 	// ------------------
 
 	// load program into memory
@@ -82,7 +88,6 @@ func (c *Chip8) Run(program []byte) {
 	// run the program, i.e. iterate through the program's opcodes and execute them
 	for opcode := c.readOpcode(c.pc); opcode != NULL_OPCODE; opcode = c.readOpcode(c.pc) {
 		c.pc += 2
-		c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("%04x\t", opcode))
 		c.exec(opcode)
 		c.refreshDisplay()
 	}
@@ -108,8 +113,10 @@ func (c *Chip8) clearDisplay() {
  */
 func (c *Chip8) drawSprite(sprite []byte, x, y uint16) bool {
 	var occluded bool = false
-	for i := int(x); i <= int(x)+len(sprite); i++ {
-		row := c.screen[i]
+	c.logger.Printf("drawing sprite of height %d from rows %d to %d", len(sprite), x, int(x)+len(sprite))
+	for i := 0; i < len(sprite); i++ {
+		offset := i + int(x)
+		row := c.screen[offset]
 		// shift sprite into position on screen by
 		// moving it at most 56 pixels left (if y=0),
 		// so that the sprite is the top byte of
@@ -118,7 +125,7 @@ func (c *Chip8) drawSprite(sprite []byte, x, y uint16) bool {
 		if row&spriteRow != 0 {
 			occluded = true
 		}
-		c.screen[i] = row ^ spriteRow
+		c.screen[offset] = row ^ spriteRow
 	}
 	return occluded
 }
@@ -166,73 +173,73 @@ func (c *Chip8) exec(opcode uint16) {
 		switch opcode {
 		// 00E0: CLS (clear)
 		case 0x00e0:
+			c.logger.Printf("%04x: CLS", opcode)
 			c.clearScr()
-			c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintln("CLS"))
 
 		// 00EE: RET (return)
 		case 0x00ee:
+			c.logger.Printf("%04x: RET", opcode)
 			c.pc = c.stack[c.sp]
 			c.sp -= 1
-			c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintln("RET"))
 
 		default:
 			panic(fmt.Sprintf("Unrecognized opcode: %04x", opcode))
 		}
 
+	// 1nnn: JP (jump) addr
 	case 0x1:
-		// 1nnn: JP (jump) addr
 		addr := opcode & 0x0fff
+		c.logger.Printf("%04x: JP %03x\n", opcode, addr)
 		c.pc = addr
-		c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("JP %03x\n", addr))
 
+	// 2nnn: CALL addr
 	case 0x2:
-		// 2nnn: CALL addr
 		addr := opcode & 0x0fff
+		c.logger.Printf("%04x: CALL %03x\n", opcode, addr)
 		c.stack = append(c.stack, c.pc)
 		c.sp += 1
 		c.pc = addr
-		c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("CALL %03x\n", addr))
 
+	// 3xkk: SE Vx byte (skip if equal)
 	case 0x3:
-		// 3xkk: SE Vx byte (skip if equal)
 		x := opcode & 0x0f00 >> 8
 		kk := opcode & 0x00ff
+		c.logger.Printf("%04x: SE V%x %02x\n", opcode, x, kk)
 		if c.v[x] == byte(kk) {
 			c.pc += 2
 		}
-		c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("SE V%x %02x\n", x, kk))
 
+	// 4xkk: SNE Vx byte (skip if not equal)
 	case 0x4:
-		// 4xkk: SNE Vx byte (skip if not equal)
 		x := opcode & 0x0f00 >> 8
 		kk := opcode & 0x00ff
+		c.logger.Printf("%04x: SNE V%x %02x\n", opcode, x, kk)
 		if c.v[x] != byte(kk) {
 			c.pc += 2
 		}
-		c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("SNE V%x %02x\n", x, kk))
 
+	// 5xy0: SE Vx Vy (skip if equal)
 	case 0x5:
-		// 5xy0: SE Vx Vy (skip if equal)
 		x := opcode & 0x0f00 >> 8
 		y := opcode & 0x00f0 >> 4
+		c.logger.Printf("%04x: SE V%x V%x\n", opcode, x, y)
 		if c.v[x] == c.v[y] {
 			c.pc += 2
 		}
-		c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("SE V%x V%x\n", x, y))
 
+	// 6xkk: LD Vx byte (load value to register)
 	case 0x6:
-		// 6xkk: LD Vx byte (load value to register)
 		x := opcode & 0x0f00 >> 8
 		kk := opcode & 0x00ff
+		c.logger.Printf("%04x: LD V%x %02x\n", opcode, x, kk)
 		c.v[x] = byte(kk)
-		c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("LD V%x %02x\n", x, kk))
 
+	// 7xkk: ADD Vx byte (add value to register)
 	case 0x7:
-		// 7xkk: ADD Vx byte (add value to register)
 		x := opcode & 0x0f00 >> 8
 		kk := opcode & 0x00ff
+		c.logger.Printf("%04x: ADD V%x %02x\n", opcode, x, kk)
 		c.v[x] = c.v[x] + byte(kk)
-		c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("ADD V%x %02x\n", x, kk))
 
 	case 0x8:
 		switch last := opcode & 0x000f; last {
@@ -241,112 +248,113 @@ func (c *Chip8) exec(opcode uint16) {
 		case 0x0:
 			x := opcode & 0x0f00 >> 8
 			y := opcode & 0x00f0 >> 4
+			c.logger.Printf("%04x: LD V%x V%x\n", opcode, x, y)
 			c.v[x] = c.v[y]
-			c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("LD V%x V%x\n", x, y))
 
 		// 8xy1: OR Vx Vy (or Vx Vy, assign result to Vx)
 		case 0x1:
 			x := opcode & 0x0f00 >> 8
 			y := opcode & 0x00f0 >> 4
+			c.logger.Printf("%04x: OR V%x V%x\n", opcode, x, y)
 			c.v[x] = c.v[x] | c.v[y]
-			c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("OR V%x V%x\n", x, y))
 
 		// 8xy2: AND Vx Vy (and Vx Vy, assign result to Vx)
 		case 0x2:
 			x := opcode & 0x0f00 >> 8
 			y := opcode & 0x00f0 >> 4
+			c.logger.Printf("%04x: OR V%x V%x\n", opcode, x, y)
 			c.v[x] = c.v[x] & c.v[y]
-			c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("OR V%x V%x\n", x, y))
 
 		// 8xy3: XOR Vx Vy (or Vx Vy, assign result to Vx)
 		case 0x3:
 			x := opcode & 0x0f00 >> 8
 			y := opcode & 0x00f0 >> 4
+			c.logger.Printf("%04x: XOR V%x V%x\n", opcode, x, y)
 			c.v[x] = c.v[x] ^ c.v[y]
-			c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("XOR V%x V%x\n", x, y))
 
 		// 8xy4: ADD Vx Vy (add Vx Vy, assign result to Vx)
 		case 0x4:
 			x := opcode & 0x0f00 >> 8
 			y := opcode & 0x00f0 >> 4
+			c.logger.Printf("%04x: ADD V%x V%x\n", opcode, x, y)
 			c.v[x] = c.v[x] + c.v[y]
-			c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("ADD V%x V%x\n", x, y))
 
 		// 8xy5: SUB Vx Vy (sub Vx Vy, assign result to Vx)
 		case 0x5:
 			x := opcode & 0x0f00 >> 8
 			y := opcode & 0x00f0 >> 4
+			c.logger.Printf("%04x: SUB V%x V%x\n", opcode, x, y)
 			c.v[x] = c.v[x] - c.v[y]
-			c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("SUB V%x V%x\n", x, y))
 
 		// 8xy6: SHR Vx Vy (set VF=1 if the lowest bit of Vx is 1 otherwise set VF=0, then right shift Vx by 1)
 		case 0x6:
 			x := opcode & 0x0f00 >> 8
 			y := opcode & 0x00f0 >> 4
+			c.logger.Printf("%04x: SHR V%x V%x\n", opcode, x, y)
 			c.v[0xf] = c.v[x] & 0x01
 			c.v[x] = c.v[x] >> 1
-			c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("SHR V%x V%x\n", x, y))
 
 		// 8xy7: SUBN Vx Vy (set VF=1 if Vy > Vx otherwise set VF=0, sub Vx Vy, assign result to Vx)
 		case 0x7:
 			x := opcode & 0x0f00 >> 8
 			y := opcode & 0x00f0 >> 4
+			c.logger.Printf("%04x: SUBN V%x V%x\n", opcode, x, y)
 			if c.v[y] > c.v[x] {
 				c.v[0xf] = 1
 			} else {
 				c.v[0xf] = 0
 			}
 			c.v[x] = c.v[x] - c.v[y]
-			c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("SUBN V%x V%x\n", x, y))
 
 		// 8xyE: SHL Vx Vy (set VF=1 if the highest bit of Vx is 1 otherwise set VF=0, then left shift Vx by 1)
 		case 0xE:
 			x := opcode & 0x0f00 >> 8
 			y := opcode & 0x00f0 >> 4
+			c.logger.Printf("%04x: SHL V%x V%x\n", opcode, x, y)
 			c.v[0xf] = c.v[x] & 0x80 // 128 in decimal, 1000 0000 in binary
 			c.v[x] = c.v[x] << 1
-			c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("SHL V%x V%x\n", x, y))
 
 		default:
 			panic(fmt.Sprintf("Unrecognized opcode: %04x", opcode))
 		}
 
+	// 9xy0: SNE Vx Vy (skip next opcode if Vx != Vy)
 	case 0x9:
-		// 9xy0: SNE Vx Vy (skip next opcode if Vx != Vy)
 		x := opcode & 0x0f00 >> 8
 		y := opcode & 0x00f0 >> 4
+		c.logger.Printf("%04x: SNE V%x V%x\n", opcode, x, y)
 		if c.v[x] != c.v[y] {
 			c.pc += 2
 		}
-		c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("SNE V%x V%x\n", x, y))
 
+	// Annn: LD I addr (set I=nnn)
 	case 0xA:
-		// Annn: LD I addr (set I=nnn)
 		addr := opcode & 0x0fff
+		c.logger.Printf("%04x: LD I %03x\n", opcode, addr)
 		c.i = addr
-		c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("LD I %03x\n", addr))
 
+	// Bnnn: JP V0 addr (jump to address nnn + v0, set PC=nnn + v0)
 	case 0xB:
-		// Bnnn: JP V0 addr (jump to address nnn + v0, set PC=nnn + v0)
 		addr := opcode & 0x0fff
+		c.logger.Printf("%04x: JP V0 %03x\n", opcode, addr)
 		c.pc = addr + uint16(c.v[0])
-		c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("JP V0 %03x\n", addr))
 
+	// Cxkk: RND Vx byte (Vx = random byte and kk)
 	case 0xC:
-		// Cxkk: RND Vx byte (Vx = random byte and kk)
 		x := opcode & 0x0f00 >> 8
 		kk := opcode & 0x00ff
+		c.logger.Printf("%04x: RND V%x %02x\n", opcode, x, kk)
 		// Read is exported function from math/rand -- loads random bytes into passed array.
 		rnd := byte(rand.Intn(256))
 		c.v[x] = rnd & byte(kk)
-		c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("RND V%x %02x\n", x, kk))
 
+	// Dxyn: DRW Vx Vy n (display n-byte sprite located at I at coordinates Vx,Vy, set VF=collision [if sprite is drawn on top of any active pixels])
 	case 0xD:
-		// Dxyn: DRW Vx Vy n (display n-byte sprite located at I at coordinates Vx,Vy, set VF=collision [if sprite is drawn on top of any active pixels])
 		x := opcode & 0x0f00 >> 8
 		y := opcode & 0x00f0 >> 4
 		n := opcode & 0x000f
-		sprite := make([]byte, 16, 16)
+		c.logger.Printf("%04x: DRW V%x V%x %x\n", opcode, x, y, n)
+		sprite := make([]byte, 0, 16)
 		for i := c.i; i < c.i+n; i++ {
 			sprite = append(sprite, c.memory[i])
 		}
@@ -356,25 +364,24 @@ func (c *Chip8) exec(opcode uint16) {
 		} else {
 			c.v[0xf] = 0
 		}
-		c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("DRW V%x V%x %x\n", x, y, n))
 
 	case 0xE:
 		switch lastTwo := opcode & 0x0ff; lastTwo {
 		// Ex9E: SKP Vx (skip next instruction if key with the value of Vx is currently pressed)
 		case 0x9E:
 			x := opcode & 0x0f00 >> 8
+			c.logger.Printf("%04x: SKP V%x\n", opcode, x)
 			if c.keyIsPressed(c.v[x]) {
 				c.pc += 2
 			}
-			c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("SKP V%x\n", x))
 
 		// ExA1: SKNP Vx (skip next instruction if key with the value of Vx is currently not pressed)
 		case 0xA1:
 			x := opcode & 0x0f00 >> 8
+			c.logger.Printf("%04x: SKNP V%x\n", opcode, x)
 			if !c.keyIsPressed(c.v[x]) {
 				c.pc += 2
 			}
-			c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("SKNP V%x\n", x))
 
 		default:
 			panic(fmt.Sprintf("Unrecognized opcode: %04x", opcode))
@@ -385,60 +392,60 @@ func (c *Chip8) exec(opcode uint16) {
 		// Fx07: LD Vx DT (set Vx=DT)
 		case 0x07:
 			x := opcode & 0x0f00 >> 8
+			c.logger.Printf("%04x: LD V%x DT\n", opcode, x)
 			c.v[x] = c.dt
-			c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("LD V%x DT\n", x))
 
 		// Fx0A: LD Vx K (wait for key press, store value of key press in Vx)
 		case 0x0a:
 			x := opcode & 0x0f00 >> 8
+			c.logger.Printf("%04x: LD V%x K\n", opcode, x)
 			// TODO implement
-			c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("LD V%x K\n", x))
 
 		// Fx15: LD DT Vx (set DT=Vx)
 		case 0x15:
 			x := opcode & 0x0f00 >> 8
+			c.logger.Printf("%04x: LD DT V%x\n", opcode, x)
 			c.dt = c.v[x]
-			c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("LD DT V%x\n", x))
 
 		// Fx18: LD ST Vx (set ST=Vx)
 		case 0x18:
 			x := opcode & 0x0f00 >> 8
+			c.logger.Printf("%04x: LD ST V%x\n", opcode, x)
 			c.st = c.v[x]
-			c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("LD ST V%x\n", x))
 
 		// Fx1E: ADD I Vx (set I=I+Vx)
 		case 0x1E:
 			x := opcode & 0x0f00 >> 8
+			c.logger.Printf("%04x: ADD I V%x\n", opcode, x)
 			c.i = c.i + uint16(c.v[x])
-			c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("ADD I V%x\n", x))
 
 		// Fx29: LD F Vx (set I=memory address of sprite corresponding to digit in Vx)
 		case 0x29:
 			x := opcode & 0x0f00 >> 8
+			c.logger.Printf("%04x: LD F V%x\n", opcode, x)
 			// TODO implement
-			c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("LD F V%x\n", x))
 
 		// Fx33: LD B Vx (store binary converted decimal [BCD] representation of number in Vx in memory locations I(hundreds place), I+1(tens place), I+2(ones place)
 		case 0x33:
 			x := opcode & 0x0f00 >> 8
+			c.logger.Printf("%04x: LD B V%x\n", opcode, x)
 			// TODO implement
-			c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("LD B V%x\n", x))
 
 		// Fx55: LD I Vx (store registers V0 through Vx in memory starting at I)
 		case 0x55:
 			x := opcode & 0x0f00 >> 8
+			c.logger.Printf("%04x: LD I V%x\n", opcode, x)
 			for i := uint16(0); i < x; i++ {
 				c.memory[c.i+i] = c.v[i]
 			}
-			c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("LD I V%x\n", x))
 
 		// Fx65: LD Vx I (read values in memory starting at I into registers V0 through Vx)
 		case 0x65:
 			x := opcode & 0x0f00 >> 8
+			c.logger.Printf("%04x: LD V%x I\n", opcode, x)
 			for i := uint16(0); i < x; i++ {
 				c.v[i] = c.memory[c.i+i]
 			}
-			c.DEBUG_INSTRS = append(c.DEBUG_INSTRS, fmt.Sprintf("LD V%x I\n", x))
 
 		default:
 			panic(fmt.Sprintf("Unrecognized opcode: %04x", opcode))
@@ -454,7 +461,7 @@ func (c *Chip8) readOpcode(addr uint16) uint16 {
 	// the opcode we want to read is the next two bytes,
 	// stored big-endian.
 	high := c.memory[addr]
-	low := c.memory[addr]
+	low := c.memory[addr+1]
 	// combine bytes as one uint16,
 	// keeping the big-endian representation
 	opcode := (uint16(high) << 8) | uint16(low)
